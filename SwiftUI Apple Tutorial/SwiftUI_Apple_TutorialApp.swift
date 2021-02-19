f614d9df67a8d6ed74e30516bd4f61956c03c0ec
Yevhenii Orlov <jacksorlen@icloud.com>
2021-02-19 23:25:41 +0200

    Initial project state

---
+++ SwiftUI Apple Tutorial/SwiftUI_Apple_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_TutorialApp.swift
+//  SwiftUI Apple Tutorial
+//
+//  Created by Yevhenii Orlov on 19.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
